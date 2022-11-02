1269ee7895b83c2cb972b37a4ee4f6578d684235
Finian O'Brien <40236828+finianobrien@users.noreply.github.com>
2022-11-02 14:50:46 +0100

    Update NotFound.js

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -26,16 +26,16 @@ export default function NotFound() {
                 <Translate
                   id="theme.NotFound.p1"
                   description="The first paragraph of the 404 page">
-                  We tried really hard but we could not find whatever you were looking for. But we are sure it is here somewhere. We have been rearanging a lot lately so it may have been lost in the moving process. But fear not we are here to help!
+                  We tried really hard but we could not find whatever you were looking for. But we are sure it is here somewhere. We have been rearranging a lot lately so it may have been lost in the moving process. Although fear not as we are here to help!
                   
-                  If you know what product it concerns you could try the top menu right at the very top. If you look to the right there is also a search option which we think can be really helpfull in times of need.
+                  If you know what product it concerns you could try the menu right at the very top. If you look to the right there is also a search option which we think might be really helpfull in times of need.
                 </Translate>
               </p>
               <p>
                 <Translate
                   id="theme.NotFound.p2"
                   description="The 2nd paragraph of the 404 page">
-                  If everything else fails you are very welcome to contact us at developer@mobilepay.dk. We will gladly help you find what it is you are looking for. Purhaps it is not even there anymore but then we will do what we can to find something else for you. 
+                  If everything else fails you are very welcome to contact us at developer@mobilepay.dk. We will gladly help you find what you are looking for or answer any questions you might have. 
                 </Translate>
               </p>
             </div>
